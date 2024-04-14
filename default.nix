1ab3a243401518e33d3ab98c8421a240feb6e3d7
Malte Neuss <malteneuss@users.noreply.github.com>
2024-04-14 13:16:35 +0200

    GIve up compiling torch

--- default.nix
+++ default.nix
@@ -1,9 +1,28 @@
 { lib, poetry2nix, python311 }:
-poetry2nix.mkPoetryApplication {
+let
+  dontCheckPython = drv: drv.overridePythonAttrs (old: { doCheck = false; });
+in
+poetry2nix.mkPoetryApplication
+{
   projectDir = ./.;
   src = lib.cleanSource ./.;
   python = python311;
   preferWheel = true;
+  overrides = poetry2nix.overrides.withDefaults (self: super: {
+    torch = super.torch.overridePythonAttrs (oldAttrs: {
+      doCheck = false;
+    });
+    numpy = super.numpy.overridePythonAttrs (oldAttrs: {
+      doCheck = false;
+    });
+    pynacl = super.pynacl.overridePythonAttrs (oldAttrs: {
+      doCheck = false;
+    });
+
+  });
+
+
+  USE_CUDA = 0;
 
   #buildInputs = [ pkgs.python38Packages.requests ];
 }
--- release.nix
+++ release.nix
@@ -2,7 +2,16 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
   poetry2nix = pkgs.callPackage sources.poetry2nix { };
-  python-app = pkgs.callPackage ./default.nix { inherit poetry2nix; };
+  mypoetry2nix = poetry2nix.overrideScope (self: super: {
+
+    # pyself & pysuper refers to python packages
+    defaultPoetryOverrides = super.defaultPoetryOverrides.extend (pyself: pysuper: {
+      
+    });
+
+  });
+
+  python-app = pkgs.callPackage ./default.nix { poetry2nix= mypoetry2nix; };
   docker-hello = pkgs.dockerTools.buildLayeredImage {
     name = "myapp";
     tag = "latest";
